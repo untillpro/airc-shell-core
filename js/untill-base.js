ccb23bd3cb6b45514b41ecf20717662a5d872193
Evgenii Saigachenko <yohanson555@gmail.com>
2019-03-19 14:34:07 +0300

    initial commit

---
+++ js/untill-base.js
@@ -0,0 +1,16 @@
+const initPassViewToggler = () => {
+    $(".form-row-field-pass-tgl").on("click", function() {
+        const passInput = $(this).prev('input');
+
+        if (passInput) {
+            $(this).toggleClass('active');
+            const type = $(passInput).attr('type');
+            $(passInput).attr('type', type === 'password' ? 'text' : 'password');
+        }
+    })
+};
+
+
+$(() => {
+    initPassViewToggler(); //init form password input toggler
+});
