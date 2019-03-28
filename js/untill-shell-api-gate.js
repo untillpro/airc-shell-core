ac67ecaef74d6015f798801219855e7528dec296
Evgenii Saigachenko <yohanson555@gmail.com>
2019-03-28 17:39:08 +0300

    added

---
+++ js/untill-shell-api-gate.js
@@ -0,0 +1,70 @@
+
+class API {
+    static init() {
+        if (iframeApi && typeof iframeApi === 'function') {
+            iframeApi({}).then(function (api) {
+                api.changePath(window.location.href);
+            });
+        } else {
+            throw new Error('Ushell API error: iframeApi is not defined!');
+        }
+    }
+
+    static async sendRequest(action, params) {
+        if (iframeApi && typeof iframeApi === 'function') {
+            iframeApi({}).then(function (api) {
+                const token = api.getToken();
+
+                if (token) {
+                    return fetch(action, {})
+                        .then((response) => response.json());
+                } else {
+                    throw new Error('SendRequest error: token is ' + token);
+                }
+            });
+        } else {
+            throw new Error('Ushell API error: iframeApi is not defined!');
+        }
+    }
+
+    static sendError(text = null, descr = null, lifetime = 10, hideClose = false) {
+        if (iframeApi && typeof iframeApi === 'function') {
+            iframeApi({}).then(function (api) {
+                api.sendError(text, descr, lifetime, hideClose);
+            });
+        }
+    }
+
+    static sendWarning(text = null, descr = null, lifetime = 10, hideClose = false) {
+        if (iframeApi && typeof iframeApi === 'function') {
+            iframeApi({}).then(function (api) {
+                api.sendWarning(text, descr, lifetime, hideClose);
+            });
+        }
+    }
+
+    static sendSuccess(text = null, descr = null, lifetime = 10, hideClose = false) {
+        if (iframeApi && typeof iframeApi === 'function') {
+            iframeApi({}).then(function (api) {
+                api.sendSuccess(text, descr, lifetime, hideClose);
+            });
+        }
+    }
+
+    static sendInfo(text = null, descr = null, lifetime = 10, hideClose = false) {
+        if (iframeApi && typeof iframeApi === 'function') {
+            iframeApi({}).then(function (api) {
+                api.sendInfo(text, descr, lifetime, hideClose);
+            });
+        }
+    }
+}
+
+
+$(() => {
+    try {
+        API.init();
+    } catch (e) {
+        
+    }
+});
