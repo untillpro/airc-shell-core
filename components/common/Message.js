b0da2236e17c2006a220564e15335e379be3d9ec
Evgenii Saigachenko <yohanson555@gmail.com>
2019-06-13 14:54:42 +0300

    upd

--- components/common/Message.js
+++ components/common/Message.js
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Message extends Component {
+    renderHeader() {
+        if (!this.props.header) return null;
+
+        return (
+            <div class="ushell-message-block-header">
+                {this.props.header}
+            </div>
+        );
+    }
+
+    renderFooter() {
+        if (!this.props.footer) return null;
+
+        return (
+            <div class="ushell-message-block-footer">
+                {this.props.footer}
+            </div>
+        );
+    }
+
+    renderContent() {
+        return (
+            <div class="ushell-message-block-content">
+                {this.props.children}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className={`ushell-message-block ${this.props.type || ''}`}>
-                {this.props.children}
+                {this.renderHeader()}
+                {this.renderContent()}
+                {this.renderFooter()}
             </div>
         );
     }
