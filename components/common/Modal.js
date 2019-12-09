07646a60f9ed2f3f88ccc2fb2e34c3c63a8577c5
Evgenii Saigachenko <yohanson555@gmail.com>
2019-12-09 15:46:41 +0300

    chgd

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -9,11 +9,7 @@ class Modal extends AntdModal {
         switch (size) {
             case 'small': result = '__small'; break; 
             case 'large': result = '__large'; break;
-<<<<<<< HEAD
             default: result = '__medium'; break;
-=======
-            default: result = '__medium'; 
->>>>>>> 6128527331bd2646d6e9c944a35bd62a965ce8a3
         }
 
         return `${className} ${result}`;
