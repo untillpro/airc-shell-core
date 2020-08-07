216be62c0f564983ff401d32a27232a4126948f9
Yohanson555 <yohanson555@gmail.com>
2020-08-07 11:01:33 +0300

    datetime filter added

--- components/common/DateTimeFilter.js
+++ components/common/DateTimeFilter.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { Tabs, DatePicker, Button, Empty } from 'antd';
+import { DatetimeRangePicker } from 'rc-datetime-picker';
 import moment from 'moment';
 
+import "rc-datetime-picker/dist/picker.css";
 
 const { RangePicker } = DatePicker;
 const { TabPane } = Tabs;
@@ -26,23 +28,12 @@ Props:
 - error 
 - errorMessage 
 
+- showCustom
+- customTitle
 */
 
-const periods = [
-    {
-        "code": "weekly",
-        "name": "Weekly",
-        "order": 1,
-        "periods": [
-            {
-                "code": "",
-                "name": "",
-                "from": () => moment(),
-                "to": () => moment()
-            }
-        ]
-    }
-];
+const CUSTOM_DEFAULT_NAME = "Custom";
+const CUSTOM_TAB_CODE = "custom";
 
 class DateTimeFilter extends Component {
     constructor(props) {
@@ -51,8 +42,11 @@ class DateTimeFilter extends Component {
         this.state = {
             tabs: [],
             currentTab: null,
+
             from: null,
-            to: null
+            to: null,
+
+            isCustom: false,
         };
 
         this.handleSelectPeriod = this.handleSelectPeriod.bind(this);
@@ -61,30 +55,47 @@ class DateTimeFilter extends Component {
     }
 
     componentDidMount() {
-        const { from, to } = this.props;
-        
-        this.setState({
-            from: from ? moment(from) : null, 
-            to: to ? moment(to) : null
-        });
+        const { from, to, unix, showCustom } = this.props;
+
+        if (unix) {
+            this.setState({
+                isCustom: !!showCustom,
+                from: from ? moment.unix(from) : null,
+                to: to ? moment.unix(to) : null
+            });
+        } else {
+            this.setState({
+                isCustom: !!showCustom,
+                from: from ? moment(from) : null,
+                to: to ? moment(to) : null
+            });
+        }
     }
 
     handleSelectPeriod(period) {
-        const { onPeriodSelected } = this.props;
+        const { onPeriodSelected, fromTime, toTime } = this.props;
 
-        let from = null
-        let to = null
+        let from = null;
+        let to = null;
 
         if (period) {
             if (period.from && typeof period.from === 'function') {
-                from = period.from()
+                from = period.from();
+
+                if (fromTime && typeof fromTime === 'object') {
+                    from.hour(fromTime.hour()).minute(fromTime.minute()).second(0).millisecond(0);
+                }
             }
 
             if (period.to && typeof period.to === 'function') {
-                to = period.to()
+                to = period.to();
+
+                if (toTime && typeof toTime === 'object') {
+                    to.hour(toTime.hour()).minute(toTime.minute()).second(0).millisecond(0);
+                }
             }
 
-            this.setState({ from, to });
+            this.handleChange([from, to]);
         }
 
         if (onPeriodSelected && typeof onPeriodSelected === 'function') {
@@ -93,47 +104,96 @@ class DateTimeFilter extends Component {
     }
 
     handleChange(dates) {
-        const { onChange } = this.props;
-        const [ from, to ] = dates;
+        const { onChange, unix } = this.props;
+        const [from, to] = dates;
 
         this.setState({ from, to });
 
         if (onChange && typeof onChange === 'function') {
-            onChange({
-                "from": from ? from.unix() : null, 
-                "to": to ? to.unix() : null,
-            })
+            if (unix) {
+                onChange({
+                    "from": from ? from.unix() : null,
+                    "to": to ? to.unix() : null,
+                })
+            } else {
+                onChange({
+                    "from": from ? from.valueOf() : null,
+                    "to": to ? to.valueOf() : null,
+                })
+            }
+
         }
     }
 
     handleTabChange(tab) {
+        const { isCustom } = this.state;
         const { onTabChange } = this.props;
 
-        if (onTabChange && typeof onTabChange === 'function') {
-            onTabChange(tab)
+        console.log('handle tab change', tab);
+
+        if (tab === CUSTOM_TAB_CODE) {
+            if (!isCustom) {
+                this.setState({ isCustom: true });
+            }
+        } else {
+            if (onTabChange && typeof onTabChange === 'function') {
+                onTabChange(tab)
+            }
+
+            if (isCustom) {
+                this.setState({ isCustom: false });
+            }
         }
     }
 
-    rederPeriods() {
-        const { periods } = this.props;
+    getPeriods() {
+        const { periods, showCustom, customTitle } = this.props;
 
-        console.log('I have this periods');
-        console.log(periods);
+        let result = [];
+
+        if (showCustom === true) {
+            result.push({
+                name: customTitle || CUSTOM_DEFAULT_NAME,
+                code: CUSTOM_TAB_CODE
+            });
+        }
+
+        if (periods && _.isArray(periods) && periods.length > 0) {
+            result = _.concat(result, periods)
+        }
+
+        return result;
+    }
+
+    rederPeriods() {
+        const periods = this.getPeriods();
+
+        console.log('periods is : ', periods);
 
         if (_.size(periods) > 0) {
             return (
                 <div className="date-time-filter-periods">
-                    <Tabs 
-                        defaultActiveKey={_.first(periods).code} 
-                        animated={false} 
+                    <Tabs
+                        defaultActiveKey={_.first(periods).code}
+                        animated={false}
                         onChange={this.handleTabChange}
                     >
                         {periods.map((p) => {
+                            if (p.code === CUSTOM_TAB_CODE) {
+                                return <TabPane tab={p.name} key={p.code}></TabPane>
+                            }
+
                             return (
                                 <TabPane tab={p.name} key={p.code}>
-                                    {p.periods && _.isArray(p.periods) && p.periods.length > 0 ? 
-                                    p.periods.map(period => <Button size={"small"} onClick={() => this.handleSelectPeriod(period)}>{period.name}</Button>)
-                                    : <Empty description="No periods found" />}
+                                    {p.periods && _.isArray(p.periods) && p.periods.length > 0 ?
+                                        p.periods.map(period => {
+                                            return (
+                                                <Button key={`${period.code}`} size={"small"} onClick={() => this.handleSelectPeriod(period)}>
+                                                    {period.name}
+                                                </Button>
+                                            );
+                                        })
+                                        : <Empty description="No periods found" />}
                                 </TabPane>
                             )
                         })}
@@ -145,17 +205,27 @@ class DateTimeFilter extends Component {
         return null;
     }
 
-    renderCalendar() {
-        const {from, to} = this.state;
+    renderPicker() {
+        const { from, to } = this.state;
 
         return (
             <div className="date-time-filter-pickers">
-                <RangePicker 
+                <RangePicker
+                    disabled={true}
                     allowEmpty={[true, true]}
                     value={[from, to]}
                     onChange={this.handleChange}
-                    showTime 
+                    showTime
+                    format={"YYYY-MM-DD HH:mm"}
                 />
+            </div>
+        );
+    }
+
+    renderCalendars() {
+        return (
+            <div>
+                <DatetimeRangePicker showTimePicker shortcuts />
             </div>
         );
     }
@@ -175,11 +245,14 @@ class DateTimeFilter extends Component {
     }
 
     render() {
+        const { isCustom } = this.state;
 
         return (
             <div className="date-time-filter">
                 {this.rederPeriods()}
-                {this.renderCalendar()}
+
+                {isCustom === true ? this.renderCalendars() : this.renderPicker()}
+
                 {this.renderError()}
             </div>
         );
