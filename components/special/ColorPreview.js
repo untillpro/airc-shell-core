820a27b89fb21fedc9cec2d4be2c47add6c6887a
Evgeniy Saigachenko <yohanson555@gmail.com>
2021-04-21 15:05:19 +0300

    conv import fix

--- components/special/ColorPreview.js
+++ components/special/ColorPreview.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import conv from 'color-shorthand-hex-to-six-digit';
+import { conv } from 'color-shorthand-hex-to-six-digit';
 
 const ColorPreview = (props) => {
     return (
